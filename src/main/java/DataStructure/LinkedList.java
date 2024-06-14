5ac5cfbdbdc3c318778725a0c671331fd0ec5090
Victor Moreira <victor_farfam@hotmail.com>
2024-06-14 15:03:22 -0300

    Criação de Main na LinkedList, criação da primeira pilha

--- src/main/java/DataStructure/LinkedList.java
+++ src/main/java/DataStructure/LinkedList.java
@@ -180,3 +180,30 @@ public class LinkedList {
     }
 
 }
+/*public class Main {
+    public static void main(String[] args) {
+        DataStructure.LinkedList list = new LinkedList("elemento 1");
+        list.append("elemento 2");
+        list.append("elemento 3");
+        list.prepend("Elemento 0");
+
+        list.remove(2);
+        list.print();
+
+        list.insert(3, "elemento 2.5");
+
+        System.out.println(list.get(2).data);
+
+        System.out.println(list.removeFirst().data);
+        list.print();
+
+        list.set(1, "elemento 0.5");
+        list.print();
+
+        list.getHead();
+        list.getTail();
+        list.getLength();
+        list.print();
+
+    }
+}*/
