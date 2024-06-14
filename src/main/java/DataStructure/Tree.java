9786ce7feb10d2a1f2184f10b8a56241871ad107
Victor Moreira <victor_farfam@hotmail.com>
2024-06-14 20:40:27 -0300

    Criação do em ordem no Tree.java

--- src/main/java/DataStructure/Tree.java
+++ src/main/java/DataStructure/Tree.java
@@ -59,6 +59,26 @@ public class Tree {
         preOrder(node.right);
     }
 
+    //Em ordem
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(final Node node) {
+        if(node == null) return;
+        inOrder(node.left);
+        System.out.println(node.value);
+        inOrder(node.right);
+    }
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
 
@@ -73,7 +93,7 @@ public class Tree {
 
 
         System.out.println("#####################");
-        tree.preOrder();
+        tree.inOrder();
         System.out.println("#####################");
 
 
