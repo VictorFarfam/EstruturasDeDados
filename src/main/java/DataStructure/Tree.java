395024122fa462638cae8eed4f75eadb4f99c190
Victor Moreira <victor_farfam@hotmail.com>
2024-06-14 20:32:34 -0300

    Criação do pre ordem no Tree.java

--- src/main/java/DataStructure/Tree.java
+++ src/main/java/DataStructure/Tree.java
@@ -47,9 +47,51 @@ public class Tree {
         }
     }
 
+    //pre ordem
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(final Node node) {
+        if(node == null) return;
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
 
     public static void main(String[] args) {
+
         Tree tree = new Tree();
+        tree.insert(37);
+        tree.insert(11);
+        tree.insert(66);
+        tree.insert(8);
+        tree.insert(17);
+        tree.insert(42);
+        tree.insert(72);
+
+
+        System.out.println("#####################");
+        tree.preOrder();
+        System.out.println("#####################");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Tree tree = new Tree();
         tree.insert(1);
         tree.insert(3);
         tree.insert(2);
@@ -59,7 +101,7 @@ public class Tree {
         System.out.println(tree.root.right.value);
 
         System.out.println(tree.root.isLeaf());
-        System.out.println(tree.root.right.isLeaf());
+        System.out.println(tree.root.right.isLeaf());*/
     }
 
 
