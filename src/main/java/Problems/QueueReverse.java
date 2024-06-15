bd98338e82da6eac6de89501b6d1092759696fc6
Victor Moreira <victor_farfam@hotmail.com>
2024-06-14 22:47:15 -0300

    Resolução de exercicios

---
+++ src/main/java/Problems/QueueReverse.java
@@ -0,0 +1,38 @@
+package Problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueReverse {
+
+    public static Queue<Integer> reverseFirstK(Queue<Integer> queue, int k) {
+        solveQueue(queue, k);
+        int n = queue.size() - k;
+        while (n-- > 0 ) {
+            int elemente = queue.remove();
+            queue.add(elemente);
+        }
+        return queue;
+    }
+
+    private static void solveQueue(final Queue<Integer> queue, final int k) {
+        if (k == 0) return;
+        int element = queue.remove();
+        solveQueue(queue, k-1);
+        queue.add(element);
+    }
+
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        System.out.println(reverseFirstK(queue, 3));
+    }
+
+
+
+}
